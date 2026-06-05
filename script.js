4954353faa52b58bc781417181d0093d2526e394
Ankit <ankitredeemcode72@gmail.com>
2026-06-05 21:45:38 +0530

    Day 2 Todo UI design

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function addTask() {
-    let task = document.getElementById("taskInput").value;
 
-    let li = document.createElement("li");
+    let task =
+    document.getElementById("taskInput").value;
+
+    if(task===""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li =
+    document.createElement("li");
+
+    li.className =
+    "list-group-item";
+
     li.innerText = task;
 
-    document.getElementById("taskList").appendChild(li);
+    document
+    .getElementById("taskList")
+    .appendChild(li);
+
+    document
+    .getElementById("taskInput")
+    .value = "";
 }
